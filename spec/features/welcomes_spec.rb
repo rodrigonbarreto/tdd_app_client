1127535ebf4064c931561b9ef4a3d0c4ac7612dd
Rodrigo Barreto <rodrigonbarreto@gmail.com>
2018-02-18 13:34:32 +0100

    welcome first test

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 #RSpec.feature "Welcomes", type: :feature do -> if you dant to open navigator don't use js: true
 RSpec.feature "Welcomes", type: :feature, js: true do
-    scenario 'show welcome message' do
-     visit('/')
-     expect(page).to have_content("Welcomes")
-     sleep(10)
-    end
+  scenario 'show welcome message' do
+   visit(root_path)
+   expect(page).to have_content("Welcomes")
+   sleep(3)
+  end
+  scenario 'check client link' do
+    visit(root_path)
+
+    expect(page).to have_selector('ul li')
+    expect(find('ul li')).to have_link('Clients')
+    sleep(3)
+  end
 end
